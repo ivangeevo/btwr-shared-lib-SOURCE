47cd75636ce220eff0eeabce8a46b2abd5d749bf
Ivan Geevo <dbowgaming@gmail.com>
2024-11-18 11:18:48 +0200

    init without gradle cache

---
+++ src/main/java/btwr/BTWRSharedLib/BtwrSharedLib.java
@@ -0,0 +1,11 @@
+package btwr.BTWRSharedLib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BtwrSharedLib implements ModInitializer
+{
+
+    @Override
+    public void onInitialize() {
+    }
+}

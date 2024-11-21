df575b81c33f2877a354672cfbbfe2d2e01e989b
Ivan Geevo <dbowgaming@gmail.com>
2024-11-21 18:21:28 +0200

    init sources

---
+++ src/main/java/btwr/lib/BTWRSLMod.java
@@ -0,0 +1,12 @@
+package btwr.lib;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BTWRSLMod implements ModInitializer
+{
+
+    @Override
+    public void onInitialize() {}
+
+
+}
---
+++ src/main/java/btwr/lib/mixin/BlockMixin.java
@@ -0,0 +1,32 @@
+package btwr.lib.mixin;
+
+import btwr.lib.added.BlockAdded;
+import net.minecraft.block.Block;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraft.world.WorldAccess;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(Block.class)
+public abstract class BlockMixin implements BlockAdded
+{
+    @Override
+    public void notifyOfFullStagePlantGrowthOn(World world, BlockPos pos, Block plantBlock) {}
+
+    /**
+     * This is used by old style non-daily plant growth
+     */
+    public float getPlantGrowthOnMultiplier(World world, BlockPos pos, Block plantBlock) { return 1F; }
+
+    @Override
+    public boolean isBlockHydratedForPlantGrowthOn(World world, BlockPos pos) {return false;}
+
+    @Override
+    public int getWeedsGrowthLevel(WorldAccess blockAccess, BlockPos pos)
+    {
+        return 0;
+    }
+
+    @Override
+    public void removeWeeds(World world, BlockPos pos) {}
+}

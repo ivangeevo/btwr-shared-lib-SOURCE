730d84fa35607741bd787dccd498583911c627ef
Ivan Geevo <dbowgaming@gmail.com>
2024-11-28 01:23:20 +0200

    update sources

---
+++ src/main/java/btwr/lib/interfaces/PlayerEntityAdded.java
@@ -0,0 +1,14 @@
+package btwr.lib.interfaces;
+
+public interface PlayerEntityAdded {
+
+
+    // ---------- Class level injects ---------- //
+    int timesCraftedThisTick();
+
+    void setTimesCraftedThisTick(int value);
+
+
+
+    // ---------- Regular variables ---------- //
+}
---
+++ src/main/java/btwr/lib/mixin/PlayerEntityMixin.java
@@ -0,0 +1,30 @@
+package btwr.lib.mixin;
+
+import btwr.lib.interfaces.PlayerEntityAdded;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin implements PlayerEntityAdded
+{
+
+    private int timesCraftedThisTick = 0;
+
+    @Override
+    public int timesCraftedThisTick() {
+        return timesCraftedThisTick;
+    }
+
+    @Override
+    public void setTimesCraftedThisTick(int value) {
+        timesCraftedThisTick = value;
+    }
+
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void injectedTick(CallbackInfo ci) {
+        setTimesCraftedThisTick(0);
+    }
+}

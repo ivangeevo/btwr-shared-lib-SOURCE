3214d427004e216ebb7ec753bff5eca9948d850a
Ivan Geevo <dbowgaming@gmail.com>
2024-12-03 16:38:53 +0200

    update source 0.13-SNAPSHOT

--- ## CHANGELOG.md
+++ ## CHANGELOG.md
@@ -1,21 +1,24 @@
-## v0.12
+## v0.13-SNAPSHOT
+- Added more methods to RecipeProviderUtils class
+
+## v0.12-SNAPSHOT
 -Reworked the directional dropping injection code to be less intrusive
 
 
-## v0.11
+## v0.11-SNAPSHOT
 - Added RecipeProviderUtils class
 - Refactored the StackDroppingManager class to better inject its code without breaking the original implementation
 
-## v0.10
+## v0.10-SNAPSHOT
 - Added Disabled Recipe type
 
-## v0.4
+## v0.4-SNAPSHOT
 - Added PlaceableAsBlock logic.
 
-## v0.2
+## v0.2-SNAPSHOT
 - Moved BTWRConventionalTags to this library mod
 
-## v0.1
+## v0.1-SNAPSHOT
 Initial release
 - Added BlockAdded interface
 - Added PlayerEntityAdded interface
--- src/main/java/btwr/btwrsl/lib/util/utils/RecipeProviderUtils.java
+++ src/main/java/btwr/btwrsl/lib/util/utils/RecipeProviderUtils.java
@@ -1,10 +1,12 @@
 package btwr.btwrsl.lib.util.utils;
 
 import btwr.btwrsl.lib.recipe.DisabledRecipe;
+import net.minecraft.block.Block;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.registry.Registries;
+import net.minecraft.registry.tag.TagKey;
 import net.minecraft.util.Identifier;
 
 /** Utility interface for shared recipe methods */
@@ -66,5 +68,40 @@ public interface RecipeProviderUtils {
 
     default Item grabRaw(String namespace, String itemID) {
         return Registries.ITEM.get(Identifier.of(namespace, itemID));
+    }
+
+    /** Helper method to extract wood type from an item's translation key **/
+    private String extractName(Item item) {
+        String[] parts = item.getTranslationKey().split("\\.");
+        return parts[parts.length - 1];
+    }
+
+    private String extractName(Block block) {
+        String[] parts = block.getTranslationKey().split("\\.");
+        return parts[parts.length - 1];
+    }
+
+    private String extractName(TagKey<?> tag) {
+        String[] parts = tag.getTranslationKey().split("\\.");
+        return parts[parts.length - 1];
+    }
+
+    class ID
+    {
+        static Identifier ofMC(String item) { return Identifier.ofVanilla(item); }
+        /** DS - Datapack Suite **/
+        static Identifier ofDS(String item) { return Identifier.of("btwr-ds", item); }
+        /** BTWR: Core **/
+        static Identifier ofBTWR(String item) { return Identifier.of("btwr", item); }
+        /** BWT - Better With Time **/
+        static Identifier ofBWT(String item)
+        {
+            return Identifier.of("bwt", item);
+        }
+        static Identifier ofTE(String item) { return Identifier.of("tough_environment", item); }
+        static Identifier ofST(String item) { return Identifier.of("sturdy_trees", item); }
+        static Identifier ofSS(String item) { return Identifier.of("self_sustainable", item); }
+        static Identifier ofVG(String item) { return Identifier.of("vegehenna", item); }
+
     }
 }

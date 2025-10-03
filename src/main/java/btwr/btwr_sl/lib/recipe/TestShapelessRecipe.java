ae5d3fb0ac1bbfd2e3b6b8c8025739d683de614c
ivan <dbowgaming@gmail.com>
2025-10-03 22:34:23 +0300

    Working test crafting recipe with added logic in CraftingResultSlot for handling remainder in the crafting grid for TestCraftingRecipe type.

--- src/main/java/btwr/btwr_sl/lib/recipe/TestShapelessRecipe.java
+++ src/main/java/btwr/btwr_sl/lib/recipe/TestShapelessRecipe.java
@@ -1,6 +1,7 @@
 package btwr.btwr_sl.lib.recipe;
 
 import btwr.btwr_sl.lib.mixin.accessors.ShapelessRecipeJsonBuilderAccessorMixin;
+import btwr.btwr_sl.tag.BTWRConventionalTags;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.MapCodec;
@@ -12,18 +13,17 @@ import net.minecraft.advancement.criterion.RecipeUnlockedCriterion;
 import net.minecraft.data.server.recipe.CraftingRecipeJsonBuilder;
 import net.minecraft.data.server.recipe.RecipeExporter;
 import net.minecraft.data.server.recipe.ShapelessRecipeJsonBuilder;
-import net.minecraft.item.ItemConvertible;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import net.minecraft.network.RegistryByteBuf;
 import net.minecraft.network.codec.PacketCodec;
-import net.minecraft.recipe.Ingredient;
-import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.recipe.ShapelessRecipe;
+import net.minecraft.recipe.*;
 import net.minecraft.recipe.book.CraftingRecipeCategory;
 import net.minecraft.recipe.book.RecipeCategory;
+import net.minecraft.recipe.input.CraftingRecipeInput;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.collection.DefaultedList;
 
+import java.util.List;
 import java.util.Objects;
 
 public class TestShapelessRecipe extends ShapelessRecipe {
@@ -33,44 +33,74 @@ public class TestShapelessRecipe extends ShapelessRecipe {
     }
 
     @Override
+    public RecipeType<?> getType() {
+        return BTWRSLRecipes.TEST_SHAPELESS_RECIPE_TYPE;
+    }
+
+    @Override
     public RecipeSerializer<?> getSerializer() {
         return BTWRSLRecipes.TEST_SHAPELESS_RECIPE_SERIALIZER;
     }
 
+    public ItemStack getResult() {
+        return this.getResult(null);
+    }
+
+    @Override
+    public DefaultedList<ItemStack> getRemainder(CraftingRecipeInput input) {
+        DefaultedList<ItemStack> defaultedList = DefaultedList.ofSize(input.getSize(), ItemStack.EMPTY);
+
+        for (int i = 0; i < defaultedList.size(); i++) {
+            ItemStack stack = input.getStackInSlot(i);
+            if (stack.isIn(BTWRConventionalTags.Items.DAMAGE_ON_CRAFTING)) {
+                if (stack.getDamage() < stack.getMaxDamage() - 1) {
+                    ItemStack moreDamaged = stack.copy();
+                    moreDamaged.setDamage(stack.getDamage() + 1);
+                    defaultedList.set(i, moreDamaged);
+                }
+            }
+        }
+
+        return defaultedList;
+    }
 
     public static class Serializer implements RecipeSerializer<TestShapelessRecipe> {
         private static final MapCodec<TestShapelessRecipe> CODEC = RecordCodecBuilder.mapCodec(
                 instance -> instance.group(
-                        Codec.STRING
-                                .optionalFieldOf("group", "")
-                                .forGetter(ShapelessRecipe::getGroup),
-                        CraftingRecipeCategory.CODEC
-                                .fieldOf("category")
-                                .orElse(CraftingRecipeCategory.MISC)
-                                .forGetter(ShapelessRecipe::getCategory),
-                        ItemStack.VALIDATED_CODEC
-                                .fieldOf("result")
-                                .forGetter(r -> r.getResult(null)),
-                        Ingredient.DISALLOW_EMPTY_CODEC
-                                .listOf()
-                                .fieldOf("ingredients")
-                                .flatXmap(ingredients -> {
-                                    Ingredient[] ingredients2 = ingredients.stream().filter(ingredient -> !ingredient.isEmpty()).toArray(Ingredient[]::new);
-                                    if (ingredients2.length == 0) {
-                                        return DataResult.error(() -> "No ingredients for shapeless recipe");
-                                    }
-                                    if (ingredients2.length > 16) {
-                                        return DataResult.error(() -> "Too many ingredients for shapeless recipe");
-                                    }
-                                    return DataResult.success(DefaultedList.copyOf(Ingredient.EMPTY, ingredients2));
-                                }, DataResult::success)
-                                .forGetter(ShapelessRecipe::getIngredients)
-                ).apply(instance, TestShapelessRecipe::new)
+                                Codec.STRING
+                                        .optionalFieldOf("group", "")
+                                        .forGetter(ShapelessRecipe::getGroup),
+                                CraftingRecipeCategory.CODEC
+                                        .fieldOf("category")
+                                        .orElse(CraftingRecipeCategory.MISC)
+                                        .forGetter(ShapelessRecipe::getCategory),
+                                ItemStack.VALIDATED_CODEC
+                                        .fieldOf("result")
+                                        .forGetter(TestShapelessRecipe::getResult),
+                                Ingredient.DISALLOW_EMPTY_CODEC
+                                        .listOf()
+                                        .fieldOf("ingredients")
+                                        .flatXmap(Serializer::validateIngredients, DataResult::success)
+                                        .forGetter(ShapelessRecipe::getIngredients)
+                        )
+                        .apply(instance, TestShapelessRecipe::new)
         );
 
+        private static DataResult<DefaultedList<Ingredient>> validateIngredients(List<Ingredient> ingredients) {
+            Ingredient[] ingredients2 = ingredients.stream()
+                    .filter(ingredient -> !ingredient.isEmpty())
+                    .toArray(Ingredient[]::new);
+            if (ingredients2.length == 0) {
+                return DataResult.error(() -> "No ingredients for shapeless recipe");
+            } else {
+                return ingredients2.length > 9
+                        ? DataResult.error(() -> "Too many ingredients for shapeless recipe")
+                        : DataResult.success(DefaultedList.copyOf(Ingredient.EMPTY, ingredients2));
+            }
+        }
+
         public static final PacketCodec<RegistryByteBuf, TestShapelessRecipe> PACKET_CODEC = PacketCodec.ofStatic(
-                TestShapelessRecipe.Serializer::write,
-                TestShapelessRecipe.Serializer::read
+                TestShapelessRecipe.Serializer::write, TestShapelessRecipe.Serializer::read
         );
 
         @Override
@@ -97,20 +127,23 @@ public class TestShapelessRecipe extends ShapelessRecipe {
             buf.writeString(recipe.getGroup());
             buf.writeEnumConstant(recipe.getCategory());
             buf.writeVarInt(recipe.getIngredients().size());
+
             for (Ingredient ingredient : recipe.getIngredients()) {
                 Ingredient.PACKET_CODEC.encode(buf, ingredient);
             }
-            ItemStack.PACKET_CODEC.encode(buf, recipe.getResult(null));
+
+            ItemStack.PACKET_CODEC.encode(buf, recipe.getResult());
         }
     }
 
     public static class JsonBuilder extends ShapelessRecipeJsonBuilder {
+
         public JsonBuilder(RecipeCategory category, ItemConvertible output, int count) {
             super(category, output, count);
         }
 
         public static JsonBuilder create(RecipeCategory category, ItemConvertible output) {
-            return JsonBuilder.create(category, output, 1);
+            return create(category, output, 1);
         }
 
         public static JsonBuilder create(RecipeCategory category, ItemConvertible output, int count) {
@@ -127,17 +160,15 @@ public class TestShapelessRecipe extends ShapelessRecipe {
                     .rewards(AdvancementRewards.Builder.recipe(recipeId))
                     .criteriaMerger(AdvancementRequirements.CriterionMerger.OR);
             accessor.getAdvancementBuilder().forEach(builder::criterion);
-            TestShapelessRecipe shapelessRecipe = new TestShapelessRecipe(
+            TestShapelessRecipe recipe = new TestShapelessRecipe(
                     Objects.requireNonNullElse(accessor.getGroup(), ""),
                     CraftingRecipeJsonBuilder.toCraftingCategory(accessor.getCategory()),
                     new ItemStack(accessor.getOutput(), accessor.getCount()),
                     accessor.getInputs()
             );
-            exporter.accept(recipeId, shapelessRecipe, builder.build(recipeId.withPrefixedPath("recipes/" + accessor.getCategory().getName() + "/"))
-            );
+            exporter.accept(recipeId, recipe, builder.build(recipeId.withPrefixedPath("recipes/" + accessor.getCategory().getName() + "/")));
         }
+
+
     }
-
-
 }
-

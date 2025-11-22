74986d2cc821e131fcd7ece7e1d32c1b17c2344b
ivan <dbowgaming@gmail.com>
2025-11-22 16:44:03 +0200

    bump cleanup

--- src/main/java/org/btwr/shared_library/BTWRSLMod.java
+++ src/main/java/org/btwr/shared_library/BTWRSLMod.java
@@ -2,27 +2,23 @@ package org.btwr.shared_library;
 
 import org.btwr.shared_library.recipe.BTWRSLRecipes;
 import net.fabricmc.api.ModInitializer;
-import org.btwr.shared_library.sound.BTWRSounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class BTWRSLMod implements ModInitializer
 {
-
     public static final String MOD_ID = "btwr_sl";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-    public static BTWRSLMod INSTANCE;
+    public static BTWRSLMod instance;
 
     public static BTWRSLMod getInstance() {
-        return INSTANCE;
+        return instance;
     }
-
 
     @Override
     public void onInitialize() {
+        instance = this;
         //BTWRSounds.register();
         BTWRSLRecipes.register();
-        INSTANCE = this;
     }
-
 }

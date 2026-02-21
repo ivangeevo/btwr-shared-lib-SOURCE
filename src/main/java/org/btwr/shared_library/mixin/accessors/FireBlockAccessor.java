eeebc8b95221fe760c22ab8630222e55b2efadae
ivan <dbowgaming@gmail.com>
2026-02-21 23:27:59 +0200

    + Added some new classes in the mod that aren't used currently but may prove useful in the feature

---
+++ src/main/java/org/btwr/shared_library/mixin/accessors/FireBlockAccessor.java
@@ -0,0 +1,28 @@
+package org.btwr.shared_library.mixin.accessors;
+
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import net.minecraft.block.Block;
+import net.minecraft.block.FireBlock;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+/**
+ * Accessor for internal FireBlock flammability tables.
+ */
+@Mixin(FireBlock.class)
+public interface FireBlockAccessor {
+
+    /**
+     * Access to the "spread chances" table used by vanilla FireBlock.
+     */
+    @Accessor("spreadChances")
+    Object2IntMap<Block> btwr$getSpreadChances();
+
+    /**
+     * Access to the "burn chances" table used by vanilla FireBlock.
+     * This is the closest analogue to BTW's abilityToCatchFire[].
+     */
+    @Accessor("burnChances")
+    Object2IntMap<Block> btwr$getBurnChances();
+
+}
